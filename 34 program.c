257b15af909e08c080e608f58bb4ce85b5441bee
Princesingh9594 <pintubhai8098@gmail.com>
2025-09-28 19:08:04 +0530

    Create 34 program.c

---
+++ 34 program.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int num;
+    bool isPrime = true;
+
+    // Input number
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    if (num <= 1) {
+        printf("%d is not a prime number.\n", num);
+        return 0;
+    }
+
+    // Check for factors from 2 to sqrt(num)
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime)
+        printf("%d is a prime number.\n", num);
+    else
+        printf("%d is not a prime number.\n", num);
+
+    return 0;
+}
