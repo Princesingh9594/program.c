d6cf1e57d527376a8a626bea8fb6fd1a6f2fa1e9
Princesingh9594 <pintubhai8098@gmail.com>
2025-09-28 19:36:51 +0530

    Create 62 program.c

---
+++ 62 program.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int n, temp;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input elements
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Reverse the array in-place
+    int start = 0, end = n - 1;
+    while (start < end) {
+        // Swap arr[start] and arr[end]
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+
+    // Print reversed array
+    printf("Reversed array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
