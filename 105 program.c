dfc2f1daa4efbe0099cbc83c23f524b3ffcdf587
Princesingh9594 <pintubhai8098@gmail.com>
2025-11-26 00:27:39 +0530

    Add majority element finder using Boyer-Moore
    Implement Boyer-Moore Voting Algorithm to find majority element.

--- 105 program.c
+++ 105 program.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int nums[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    int candidate = nums[0];
+    int count = 1;
+
+    // Boyer-Moore Voting Algorithm to find a potential majority
+    for (int i = 1; i < n; i++) {
+        if (nums[i] == candidate) {
+            count++;
+        } else {
+            count--;
+            if (count == 0) {
+                candidate = nums[i];
+                count = 1;
+            }
+        }
+    }
+
+    // Verify if candidate is actually majority
+    count = 0;
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == candidate)
+            count++;
+    }
+
+    if (count > n / 2)
+        printf("Majority element: %d\n", candidate);
+    else
+        printf("Majority element: -1\n");
+
+    return 0;
+}
 
