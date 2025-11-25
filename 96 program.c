9f802d6ba5ea2e82ef598d0f8b8fcfa3dd522679
Princesingh9594 <pintubhai8098@gmail.com>
2025-11-25 23:36:31 +0530

    Implement word reversal in a sentence
    This program reverses the words in a given sentence while maintaining their order.

--- 96 program.c
+++ 96 program.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[1000];
+    int i, start, end;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Remove newline character if present
+    str[strcspn(str, "\n")] = '\0';
+
+    i = 0;
+    while (str[i] != '\0') {
+        // Find start of a word
+        start = i;
+
+        // Move i to the end of the word
+        while (str[i] != ' ' && str[i] != '\0') {
+            i++;
+        }
+
+        end = i - 1;
+
+        // Reverse the word in place
+        while (start < end) {
+            char temp = str[start];
+            str[start] = str[end];
+            str[end] = temp;
+            start++;
+            end--;
+        }
+
+        // Skip the space
+        if (str[i] == ' ')
+            i++;
+    }
+
+    printf("Sentence with reversed words: %s\n", str);
+
+    return 0;
+}
 
