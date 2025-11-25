0e2a205482532380ef0bf4f50df9ccde43541ed9
Princesingh9594 <pintubhai8098@gmail.com>
2025-11-26 00:25:45 +0530

    Implement pivot index finder in 103 program.c
    Add a program to find the pivot index in an array

--- 103 program.c
+++ 103 program.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int totalSum = 0;
+    for (int i = 0; i < n; i++) {
+        totalSum += arr[i];
+    }
+
+    int leftSum = 0;
+    int pivotIndex = -1;
+
+    for (int i = 0; i < n; i++) {
+        int rightSum = totalSum - leftSum - arr[i]; // sum of elements to the right
+        if (leftSum == rightSum) {
+            pivotIndex = i;
+            break;  // leftmost pivot
+        }
+        leftSum += arr[i];
+    }
+
+    printf("Pivot index: %d\n", pivotIndex);
+
+    return 0;
+}
 
