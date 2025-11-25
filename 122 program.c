58c0ebfad830225449754763f45112590eb3aa69
Princesingh9594 <pintubhai8098@gmail.com>
2025-11-26 01:45:37 +0530

    Add file reading functionality to program.c
    This program reads and prints the contents of 'info.txt'.

--- 122 program.c
+++ 122 program.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    char line[256];
+
+    // Open file in read mode
+    FILE *file = fopen("info.txt", "r");
+    if (file == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
+    printf("Contents of info.txt:\n");
+
+    // Read each line until EOF
+    while (fgets(line, sizeof(line), file) != NULL) {
+        printf("%s", line);
+    }
+
+    // Close the file
+    fclose(file);
+
+    return 0;
+}
 
