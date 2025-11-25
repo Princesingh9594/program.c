c3b04ec41d97452183dcedf455cd4f04eb2e6e5b
Princesingh9594 <pintubhai8098@gmail.com>
2025-11-26 01:43:30 +0530

    Add string case conversion program
    This program converts the first character of a string to uppercase and the subsequent characters to lowercase, following a period.

--- 120 program.c
+++ 120 program.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main() {
+    char str[500];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    int len = strlen(str);
+
+    // Convert first character to uppercase if it's a letter
+    if (len > 0 && isalpha(str[0])) {
+        str[0] = toupper(str[0]);
+    }
+
+    // Convert rest of characters after first character
+    for (int i = 1; i < len; i++) {
+        // If previous character is '.' and current is a letter, make uppercase
+        if (str[i - 1] == '.' && isalpha(str[i])) {
+            str[i] = toupper(str[i]);
+        } else {
+            str[i] = tolower(str[i]);
+        }
+    }
+
+    printf("Sentence case: %s\n", str);
+
+    return 0;
+}
 
