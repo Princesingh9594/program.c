d490efd4d3eb23cc989a823dd460ba4f71ce4817
Princesingh9594 <pintubhai8098@gmail.com>
2025-11-26 01:46:34 +0530

    Add file reading and counting functionality
    This program reads a file and counts the total number of characters, words, and lines.

--- 123 program.c
+++ 123 program.c
@@ -1,1 +1,50 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char filename[100];
+    printf("Enter the filename to read: ");
+    scanf("%s", filename);
+
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
+    int characters = 0, words = 0, lines = 0;
+    char ch;
+    int inWord = 0;
+
+    while ((ch = fgetc(file)) != EOF) {
+        characters++;  // count every character
+
+        if (ch == '\n') {
+            lines++;   // count lines
+        }
+
+        // Check word boundaries
+        if (isspace(ch)) {
+            if (inWord) {
+                words++;
+                inWord = 0;
+            }
+        } else {
+            inWord = 1;
+        }
+    }
+
+    // If last character is not space, count the last word
+    if (inWord) {
+        words++;
+    }
+
+    fclose(file);
+
+    printf("Total characters: %d\n", characters);
+    printf("Total words: %d\n", words);
+    printf("Total lines: %d\n", lines);
+
+    return 0;
+}
 
