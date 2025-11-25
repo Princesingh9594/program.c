a79fbd12cf67c5fc6c763f8d3f19cb770b9b90cc
Princesingh9594 <pintubhai8098@gmail.com>
2025-11-25 23:40:31 +0530

    Add program to generate and print all substrings
    This program takes a string input from the user and generates all possible substrings, printing each one to the console.

--- 100 program.c
+++ 100 program.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[1000];
+    int len;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Remove newline if present
+    str[strcspn(str, "\n")] = '\0';
+
+    len = strlen(str);
+
+    printf("All substrings of the string are:\n");
+
+    // Generate all substrings
+    for (int start = 0; start < len; start++) {
+        for (int end = start; end < len; end++) {
+            // Print substring from str[start] to str[end]
+            for (int k = start; k <= end; k++) {
+                printf("%c", str[k]);
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
 
