c1e08ea34a4f38f94e4d3ae61584d64b1b71fad7
Princesingh9594 <pintubhai8098@gmail.com>
2025-11-26 01:49:07 +0530

    Update 126 program.c

--- 126 program.c
+++ 126 program.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    char filename[100];
+    printf("Enter the filename: ");
+    scanf("%s", filename);
+
+    // Try opening the file in read mode
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Error: File '%s' does not exist or cannot be opened.\n", filename);
+        return 1;
+    }
+
+    printf("Contents of '%s':\n", filename);
+
+    char ch;
+    while ((ch = fgetc(file)) != EOF) {
+        putchar(ch);  // Display the file content
+    }
+
+    fclose(file);
+    return 0;
+}
 
