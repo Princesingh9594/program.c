6ae1250f7b55f01a4059885413b7e31ede85158e
Princesingh9594 <pintubhai8098@gmail.com>
2025-11-26 00:26:47 +0530

    Add pivot integer finding program
    Implement a program to find the pivot integer for a given positive integer n.

--- 104 program.c
+++ 104 program.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a positive integer n: ");
+    scanf("%d", &n);
+
+    // Total sum of numbers from 1 to n
+    int totalSum = n * (n + 1) / 2;
+    int leftSum = 0;
+    int pivot = -1;
+
+    for (int x = 1; x <= n; x++) {
+        leftSum += x;               // sum from 1 to x
+        int rightSum = totalSum - (leftSum - x); // sum from x to n
+
+        if (leftSum == rightSum) {
+            pivot = x;
+            break; // pivot found
+        }
+    }
+
+    printf("Pivot integer: %d\n", pivot);
+
+    return 0;
+}
 
