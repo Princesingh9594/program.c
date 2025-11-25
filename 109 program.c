8d984c1ba92271eda00ce64bace9c629bcbc6dfb
Princesingh9594 <pintubhai8098@gmail.com>
2025-11-26 00:31:10 +0530

    Implement sliding window algorithm for max subarray sum

--- 109 program.c
+++ 109 program.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the size of subarray k: ");
+    scanf("%d", &k);
+
+    if (k > n || k <= 0) {
+        printf("Invalid subarray size.\n");
+        return 1;
+    }
+
+    int maxSum = 0;
+    int windowSum = 0;
+
+    // Calculate sum of first window of size k
+    for (int i = 0; i < k; i++) {
+        windowSum += arr[i];
+    }
+    maxSum = windowSum;
+
+    // Slide the window
+    for (int i = k; i < n; i++) {
+        windowSum = windowSum - arr[i - k] + arr[i];
+        if (windowSum > maxSum)
+            maxSum = windowSum;
+    }
+
+    printf("Maximum sum of subarrays of size %d: %d\n", k, maxSum);
+
+    return 0;
+}
 
