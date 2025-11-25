d17784125d584b83541cd0570b57ee072c779d5a
Princesingh9594 <pintubhai8098@gmail.com>
2025-11-26 00:24:56 +0530

    Implement ceil index finder for sorted array

--- 102 program.c
+++ 102 program.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int n, x;
+
+    printf("Enter number of elements in sorted array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements in sorted order: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter x: ");
+    scanf("%d", &x);
+
+    int ceilIndex = -1;
+
+    // Traverse array to find first element >= x
+    for (int i = 0; i < n; i++) {
+        if (arr[i] >= x) {
+            ceilIndex = i;
+            break;
+        }
+    }
+
+    printf("Index of ceil of %d: %d\n", x, ceilIndex);
+
+    return 0;
+}
 
