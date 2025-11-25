5a930f1436c0a93479b2f9a4b5c3de6a8d8f7179
Princesingh9594 <pintubhai8098@gmail.com>
2025-11-25 23:37:36 +0530

    Implement initials extraction from full name
    Add functionality to print initials from a full name input.

--- 97 program.c
+++ 97 program.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char name[1000];
+    int i = 0;
+
+    printf("Enter a full name: ");
+    fgets(name, sizeof(name), stdin);
+
+    // Print the first character if it is a letter
+    if (isalpha(name[0])) {
+        printf("%c", toupper(name[0]));
+    }
+
+    // Loop through the string to find spaces
+    while (name[i] != '\0') {
+        if (name[i] == ' ' && isalpha(name[i + 1])) {
+            printf("%c", toupper(name[i + 1]));
+        }
+        i++;
+    }
+
+    printf("\n");
+    return 0;
+}
 
