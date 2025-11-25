572321e74e1125e7c59f940bdbf84ee0af8800b7
Princesingh9594 <pintubhai8098@gmail.com>
2025-11-25 23:17:53 +0530

    Implement string reversal in C program

--- 85 program.c
+++ 85 program.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    int i = 0, j, temp;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Find string length manually
+    while (str[i] != '\0') {
+        i++;
+    }
+
+    // If fgets adds newline, remove it
+    if (str[i - 1] == '\n') {
+        str[i - 1] = '\0';
+        i--;
+    }
+
+    // Reverse using two-pointer method
+    j = i - 1;          // last index
+    int start = 0;
+
+    while (start < j) {
+        temp = str[start];
+        str[start] = str[j];
+        str[j] = temp;
+
+        start++;
+        j--;
+    }
+
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
 
