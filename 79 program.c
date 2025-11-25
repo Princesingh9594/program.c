54da8524f1c1d3b5bc5d5643bd21a4200e3279ab
Princesingh9594 <pintubhai8098@gmail.com>
2025-11-25 23:09:56 +0530

    Implement matrix diagonal printing in C

---
+++ 79 program.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    printf("Enter rows and columns: ");
+    scanf("%d", &n);
+    scanf("%d", &m);
+
+    int a[n][m];
+    printf("Enter matrix elements:\n");
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            scanf("%d", &a[i][j]);
+
+    
+    for (int col = 0; col < m; col++) {
+        int i = 0, j = col;
+        while (i < n && j >= 0) {
+            printf("%d ", a[i][j]);
+            i++;
+            j--;
+        }
+        printf("\n");
+    }
+
+    // 2. Print diagonals starting from last column of remaining rows
+    for (int row = 1; row < n; row++) {
+        int i = row, j = m - 1;
+        while (i < n && j >= 0) {
+            printf("%d ", a[i][j]);
+            i++;
+            j--;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
