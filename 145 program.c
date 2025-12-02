84402f2b989d1a1f124a845b77350a6d8e6d9435
Princesingh9594 <pintubhai8098@gmail.com>
2025-12-03 00:19:33 +0530

    Update 145 program.c

--- 145 program.c
+++ 145 program.c
@@ -1,1 +1,53 @@
+#include <stdio.h>
+
+// Define structure
+struct Student {
+    char name[50];
+    int roll_no;
+    float marks;
+};
+
+// Function to find and return the top student
+struct Student findTopStudent(struct Student students[], int n) {
+    int topIndex = 0;
+    for (int i = 1; i < n; i++) {
+        if (students[i].marks > students[topIndex].marks) {
+            topIndex = i;
+        }
+    }
+    return students[topIndex];  // Return the structure
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    struct Student students[n];
+
+    // Input student details
+    for (int i = 0; i < n; i++) {
+        printf("\nEnter details for student %d:\n", i + 1);
+        printf("Name: ");
+        scanf("%s", students[i].name);
+
+        printf("Roll Number: ");
+        scanf("%d", &students[i].roll_no);
+
+        printf("Marks: ");
+        scanf("%f", &students[i].marks);
+    }
+
+    // Call function to get top student
+    struct Student top = findTopStudent(students, n);
+
+    // Print top student's details
+    printf("\nTop Student Details:\n");
+    printf("Name: %s\n", top.name);
+    printf("Roll Number: %d\n", top.roll_no);
+    printf("Marks: %.2f\n", top.marks);
+
+    return 0;
+}
 
