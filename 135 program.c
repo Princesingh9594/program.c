44039d06dacaa9388fcf8a0fb8ed34874d787f98
Princesingh9594 <pintubhai8098@gmail.com>
2025-12-03 00:07:28 +0530

    Update 135 program.c

--- 135 program.c
+++ 135 program.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+
+enum Status {
+    SUCCESS = 10,
+    FAILURE = 11,
+    TIMEOUT = 12
+};
+
+int main() {
+    enum Status result;
+
+    for (result = SUCCESS; result <= TIMEOUT; result++) {
+        switch (result) {
+            case SUCCESS:
+                printf("SUCCESS = %d\n", result);
+                break;
+
+            case FAILURE:
+                printf("FAILURE = %d\n", result);
+                break;
+
+            case TIMEOUT:
+                printf("TIMEOUT = %d\n", result);
+                break;
+        }
+    }
+
+    return 0;
+}
 
