b4267f90e52f2ab0e671732b34f7c626dda92cc4
Princesingh9594 <pintubhai8098@gmail.com>
2025-12-03 00:15:01 +0530

    Update 142 program.c

--- 142 program.c
+++ 142 program.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll_no;
+    float marks;
+};
+
+int main() {
+    struct Student students[5];
+
+    // Input data for 5 students
+    for (int i = 0; i < 5; i++) {
+        printf("Enter details for student %d:\n", i + 1);
+        printf("Name: ");
+        scanf("%s", students[i].name);
+
+        printf("Roll Number: ");
+        scanf("%d", &students[i].roll_no);
+
+        printf("Marks: ");
+        scanf("%f", &students[i].marks);
+
+        printf("\n");
+    }
+
+    // Print data of all students
+    printf("Student Details:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Student %d:\n", i + 1);
+        printf("Name: %s\n", students[i].name);
+        printf("Roll Number: %d\n", students[i].roll_no);
+        printf("Marks: %.2f\n\n", students[i].marks);
+    }
+
+    return 0;
+}
 
