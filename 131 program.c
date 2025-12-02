8bcc3620f58cc3b62eff90eae0646b9f0534370a
Princesingh9594 <pintubhai8098@gmail.com>
2025-12-03 00:02:49 +0530

    Update 131 program.c

--- 131 program.c
+++ 131 program.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+enum Day {
+    SUNDAY = 0,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+};
+
+int main() {
+    // Array of names for printing
+    const char *dayNames[] = {
+        "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY",
+        "THURSDAY", "FRIDAY", "SATURDAY"
+    };
+
+    for (int d = SUNDAY; d <= SATURDAY; d++) {
+        printf("%s = %d\n", dayNames[d], d);
+    }
+
+    return 0;
+}
 
