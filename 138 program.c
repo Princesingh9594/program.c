8a35b7a129297a976c4386cc002fa9fd0eb230c1
Princesingh9594 <pintubhai8098@gmail.com>
2025-12-03 00:09:53 +0530

    Update 138 program.c

--- 138 program.c
+++ 138 program.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+enum Role {
+    ADMIN = 1,
+    USER,
+    GUEST
+};
+
+int main() {
+
+    const char *roleNames[] = {
+        "",          // dummy index to align enum values (starts at 1)
+        "ADMIN",
+        "USER",
+        "GUEST"
+    };
+
+    printf("Role names and their integer values:\n");
+
+    for (int r = ADMIN; r <= GUEST; r++) {
+        printf("%s = %d\n", roleNames[r], r);
+    }
+
+    return 0;
+}
 
