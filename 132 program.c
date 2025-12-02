f0a086e807b59a9393b325c169111782a4827e8d
Princesingh9594 <pintubhai8098@gmail.com>
2025-12-03 00:03:49 +0530

    Update 132 program.c

--- 132 program.c
+++ 132 program.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+
+enum TrafficLight {
+    RED,
+    YELLOW,
+    GREEN
+};
+
+int main() {
+    enum TrafficLight light = RED;  // Change this to test different values
+
+    switch (light) {
+        case RED:
+            printf("Stop\n");
+            break;
+
+        case YELLOW:
+            printf("Wait\n");
+            break;
+
+        case GREEN:
+            printf("Go\n");
+            break;
+
+        default:
+            printf("Invalid traffic light value\n");
+            break;
+    }
+
+    return 0;
+}
 
