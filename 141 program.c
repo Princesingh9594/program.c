678de88695c341dd4927798ba8f2278c0e095944
Princesingh9594 <pintubhai8098@gmail.com>
2025-12-03 00:13:01 +0530

    Update 141 program.c

--- 141 program.c
+++ 141 program.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll_no;
+    float marks;
+};
+
+int main() {
+    struct Student s;
+
+    // Input student data
+    printf("Enter student's name: ");
+    scanf("%s", s.name);
+
+    printf("Enter roll number: ");
+    scanf("%d", &s.roll_no);
+
+    printf("Enter marks: ");
+    scanf("%f", &s.marks);
+
+    // Print student data
+    printf("\nStudent Details:\n");
+    printf("Name: %s\n", s.name);
+    printf("Roll Number: %d\n", s.roll_no);
+    printf("Marks: %.2f\n", s.marks);
+
+    return 0;
+}
 
